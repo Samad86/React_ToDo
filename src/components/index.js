8cd08eeb07841d882832f330b953374ac6855c10
Samad Kurbanov <mads198644@gmail.com>
2020-07-18 22:39:13 +0300

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as AddList } from "./AddList"; // экспортируем свойство default из AddList и называем его свойством AddList
+export { default as Badge } from "./Badge";
+export { default as List } from "./List";
+export { default as Tasks } from "./Tasks";
